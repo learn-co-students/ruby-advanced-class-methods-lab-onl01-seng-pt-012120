036e90bc6a5cbe52c61ec1c48f845fa183c2fa13
Dan Foley <dan@dan-foley.com>
2020-01-30 19:48:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,15 +9,63 @@ class Song
   end
 
   def save
+    binding.pry
+    # self.class aka Song
     self.class.all << self
   end
   
   def self.create
-    # the following do not pass the tests
-    # @@all << self.new 
-    # ============
-    self.class.all << self.new
-    # ============
-    # save
+    new_song = self.new
+    self.all << new_song
+    new_song
   end
+  
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end
+  
+  def self.create_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    self.all << new_song
+    new_song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    new_song = self.new
+    new_song.artist_name = filename.split(Regexp.union(["-", "."]))[0].strip
+    new_song.name = filename.split(Regexp.union(["-", "."]))[1].strip
+    new_song
+  end
+  
+  def self.create_from_filename(filename)
+      new_song = self.new
+    new_song.artist_name = filename.split(Regexp.union(["-", "."]))[0].strip
+    new_song.name = filename.split(Regexp.union(["-", "."]))[1].strip
+    self.all << new_song
+    new_song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
 end
