bea71ada90de9db7872ef7cfea4807e963413772
Timur Mishiev <timurmishiev@gmail.com>
2020-02-16 04:40:22 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -32,12 +32,9 @@ class Song
   def self.find_by_name(name)
     @@all.find{|x| x.name == name}
   end
+    
     def self.find_or_create_by_name(name)
-      #if self.find_by_name(name) == nil 
-        #self.create_by_name(name)
-      #else
-        #self.find_by_name(name)
-      #end
+      
       self.find_by_name(name) || self.create_by_name(name)
       #the above statement is saying do this(if it is true) or that (if the first thing is not true and the second thing is true)
   end
